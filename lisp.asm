902875a6ad98c7ba66fabc4471eba3c10eb05a82
Pertti Kellomaki <pertti.kellomaki@iki.fi>
2020-04-13 19:16:05 +0300

    More assembly.

--- lisp.asm
+++ lisp.asm
@@ -8,3 +8,15 @@
         PHI F
         LDI #09
         PLO F
+        LDA F
+        PHI 2
+        LDI #FF
+        PLO 2
+        LDA F
+        PHI 7
+        LDI #00
+        PLO 7
+7009:
+        BYTE #FF
+        BYTE #E0
+        BYTE #83
