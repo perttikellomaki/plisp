3ed3c54dd0362658c150e1a018e4b514fa3ef17c
Pertti Kellomaki <pertti.kellomaki@iki.fi>
2020-04-14 10:54:13 +0300

    More assembly.

--- lisp.asm
+++ lisp.asm
@@ -1,9 +1,35 @@
+;;; P-LISP vers. 1.0 for the RCA 1805, 1984-08-20 onwards
+;;; Originally written on paper in pencil and entered in hex by hand.
+;;; This is a third version of the Lisp, I think. There is an earlier
+;;; version also labeled 1.0 from early 1984, but for some reason I
+;;; decided to start over. I seem to remember an even earlier version,
+;;; but no documentation survives.
+;;;
+;;; Original comments are in capitals, some modern commentary (like this)
+;;; in mixed case.
+
         ;; TYÖSIVU 7000 ->
-        
+
+7000:
+;;; The nine bytes that follow are three long branches in the 1805
+;;; assembly (opcode 0xC0). The initialization code copies default
+;;; addresses in place of zeros. I think the rationale was that by
+;;; changing the addresses was that I could then e.g. redirect output
+;;; to the Teletype printer I had.
+        BYTE #C0                ; INPUT HOOK
+        BYTE #00
+        BYTE #00
+        BYTE #C0                ; OUTPUT HOOK
+        BYTE #00
+        BYTE #00
+        BYTE #C0                ; BREAK
+        BYTE #00
+        BYTE #00
+
 7009:
-        BYTE #FF                ; STACK PAGE (STACK=##FF)       OL. FF
-        BYTE #E0                ; ARGSTACK PAGE (ARGSTACK=##00) OL. E0
-        BYTE #83                ; LISP CELLS START PAGE         OL. 83
+        BYTE #FF                ; STACK PAGE (STACK = ##FF)       OL. FF
+        BYTE #E0                ; ARGSTACK PAGE (ARGSTACK = ##00) OL. E0
+        BYTE #83                ; LISP CELLS START PAGE           OL. 83
 	
         ;; LISP-TULKKI
 
@@ -38,6 +64,7 @@
         LDI #01                 ; RF OS. I/O HOOKS
         PLO F
 
+6024:   
         LDA E
         STR F
         INC F
@@ -47,3 +74,18 @@
         INC F
         GLO F
         XRI #0A
+        BNZ 24
+
+6030:
+        BR 38
+
+6032:
+        BYTE #E9                ; INPUT
+        BYTE #06
+        BYTE #E7                ; OUTPUT
+        BYTE #31
+        BYTE #00                ; BREAK (actual value missing in listing)
+        BYTE #00
+
+6038:
+        
