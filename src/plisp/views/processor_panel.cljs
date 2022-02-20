dc2e249a81fac9ade21c7f78912b6458a8d6e1da
Pertti Kellomaki <pertti.kellomaki@iki.fi>
2022-02-20 22:44:22 +0200

    Fiddle with processor tab layout

--- src/plisp/views/processor_panel.cljs
+++ src/plisp/views/processor_panel.cljs
@@ -3,6 +3,7 @@
    [clojure.string :as string]
    [re-frame.core :as rf]
    [reagent-mui.material.button :refer [button]]
+   [reagent-mui.material.stack :refer [stack]]
    [plisp.asm.lisp :as lisp]
    [plisp.services.processor-service :as processor-service]
    [plisp.subs :as subs]))
@@ -16,7 +17,7 @@
 
 (defn- register-cell [processor n]
   ^{:key (str "register-cell-" n)}
-  [:td
+  [:td {:style {:padding-right 30}}
    [:tt "R" (.toString n 16) ": "
     (hex (get-in processor [:R n :hi]))
     (hex (get-in processor [:R n :lo]))]])
@@ -25,9 +26,20 @@
   ^{:key (str "register-row-" n)}
   [:tr
    (map (partial register-cell processor)
-        [n (+ n 4) (+ n 8) (+ n 12)])])
+        [n (+ n 8)])])
 
-(defn- format-source-line [pc [i addr line]]
+(defn- registers [processor]
+  [:table
+   [:tbody
+    [:tr
+     [:td [:tt "D: " (hex (:D processor))]]
+     [:td [:tt "X: " (hex-digit (:X processor))]]]
+    [:tr
+     [:td [:tt "DF: " (hex-digit (:DF processor))]]
+     [:td [:tt "P: " (hex-digit (:P processor))]]]
+    (map (partial register-row processor) (range 8))]])
+
+(defn- format-source-line [[i addr line]]
   (str
    (.toString addr 16)
    (if (= i 0)
@@ -36,8 +48,7 @@
    line))
 
 (defn- source-panel [{:keys [source-line-number] :as current-instruction}]
-  (let [pc (get lisp/lisp-debug-info source-line-number)
-        relative-window (range -3 8)
+  (let [relative-window (range -4 10)
         window (map #(+ source-line-number %) relative-window)
         ;; Each line is a vector [i address source-text]
         source-lines (map vector
@@ -46,29 +57,23 @@
                                window)
                           (map #(nth lisp/lisp-source-text %)
                                window))]
-    [:pre
-     (when current-instruction
-       (->> (map (partial format-source-line pc) source-lines)
-            (string/join "\n")))]))
+     [:pre
+      (when current-instruction
+        (->> (map format-source-line source-lines)
+             (string/join "\n")))]))
 
 (defn processor-panel []
   (let [processor @(rf/subscribe [::subs/processor])
         instruction-count @(rf/subscribe [::subs/instruction-count])
         current-instruction @(rf/subscribe [::subs/current-instruction])]
     [:div
-     [:div instruction-count " instructions executed"]
      [button {:on-click #(rf/dispatch [::processor-service/run-processor-tick 1])} "Step"]
      [:hr]
-     [:table
-      [:tbody
-       [:tr
-        [:td [:tt "D: " (hex (:D processor))]]
-        [:td [:tt "DF: " (hex-digit (:DF processor))]]
-        [:td [:tt "X: " (hex-digit (:X processor))]]
-        [:td [:tt "P: " (hex-digit (:P processor))]]]
-       (map (partial register-row processor) (range 4))]]
+     [stack {:direction :row
+             :spacing 5}
+      [:div {:class :flex-item} [registers processor]]
+      [:div {:class :flex-item} [source-panel current-instruction]]]
+     [:div instruction-count " instructions executed"]
      [:hr]
      [:div "Input buffer: " (str (:input-buffer processor))]
-     [:hr]
-     [source-panel current-instruction]
      [:hr]]))
