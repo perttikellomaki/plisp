85b0fe64f56ee7e3ca0340a6e7475976a3af59b3
Pertti Kellomaki <pertti.kellomaki@iki.fi>
2022-03-05 21:07:40 +0200

    Use table for source panel

--- src/plisp/views/source_panel.cljs
+++ src/plisp/views/source_panel.cljs
@@ -1,6 +1,11 @@
 (ns plisp.views.source-panel
   (:require
    [re-frame.core :as rf]
+   [reagent-mui.icons.stop-circle :refer [stop-circle]]
+   [reagent-mui.material.table :refer [table]]
+   [reagent-mui.material.table-body :refer [table-body]]
+   [reagent-mui.material.table-cell :refer [table-cell]]
+   [reagent-mui.material.table-row :refer [table-row]]
    [plisp.asm.lisp :as lisp]
    [plisp.services.processor-service :as processor-service]
    [plisp.subs :as subs]
@@ -10,15 +15,19 @@
   (let [breakpoints @(rf/subscribe [::subs/breakpoints])
         address (get lisp/lisp-debug-info i)]
     ^{:key (str "source-" i)}
-    [:tr
-     [:td [:a {:on-click
-               #(rf/dispatch [::processor-service/set-breakpoint address])}
-           (hex-word address)]]
-     [:td (when (breakpoints address) "*")]
-     [:td (str->html "     ")]
-     [:td (:source-line source)]]))
+    [table-row {:hover true
+                :on-click
+                 #(rf/dispatch [::processor-service/set-breakpoint address])}
+     [table-cell {:style {:border-bottom :none}}
+      (hex-word address)]
+     [table-cell {:style {:border-bottom :none}}
+      (when (breakpoints address) [stop-circle])]
+     [table-cell {:style {:border-bottom :none}}
+      (str->html "     ")]
+     [table-cell {:style {:border-bottom :none}}
+      (:source-line source)]]))
 
 (defn source-panel []
-  [:table
-   [:tbody
+  [table {:size :small}
+   [table-body
     (doall (map-indexed source-line lisp/lisp-source))]])
